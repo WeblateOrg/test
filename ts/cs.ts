33f500e29632113c90a4652d9818a940003bb272
Michal Čihař <michal@cihar.com>
2013-11-27 13:40:46 +0100

    Add sample Qt ts file

---
+++ ts/cs.ts
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>Test</name>
+    <message>
+        <source>Hello, world!\n</source>
+        <translation></translation>
+    </message>
+    <message numerus="yes">
+        <source>Orangutan has %d banana(s).\n</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Try Weblate at &lt;http://demo.weblate.org/&gt;!\n</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Thank you for using Weblate.</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
