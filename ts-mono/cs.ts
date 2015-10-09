14d1dc2cfa82e41c101b7cb570f485a73ae82e1d
Michal Čihař <michal@cihar.com>
2015-10-09 11:43:22 +0200

    Add testcase for monolingual TS files
    See https://github.com/nijel/weblate/issues/460
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ ts-mono/cs.ts
@@ -0,0 +1,25 @@
+
+<!DOCTYPE TS>
+<TS version="2.0" language="en">
+<context>
+    <name>Weblate</name>
+    <message>
+        <source>Hello</source>
+        <translation type="unfinished"></translation>
+</translation>
+    </message>
+    <message>
+        <source>Orangutan<source>
+        <translation type="unfinished"></translation>
+</translation>
+    </message>
+    <message>
+        <source>Try<source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Thanks</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ ts-mono/en.ts
@@ -0,0 +1,24 @@
+<!DOCTYPE TS>
+<TS version="2.0" language="en">
+<context>
+    <name>Weblate</name>
+    <message>
+        <source>Hello</source>
+        <translation>Hello, world!
+</translation>
+    </message>
+    <message>
+        <source>Orangutan<source>
+        <translation>Orangutan has %d banana(s).
+</translation>
+    </message>
+    <message>
+        <source>Try<source>
+        <translation>Try Weblate at &lt;http://demo.weblate.org/&gt;!</translation>
+    </message>
+    <message>
+        <source>Thanks</source>
+        <translation>Thank you for using Weblate.</translation>
+    </message>
+</context>
+</TS>
