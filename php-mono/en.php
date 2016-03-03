323ea98d425247b2fd00a4ef23e6a825231cde0a
Michal Čihař <michal@cihar.com>
2016-03-03 10:43:43 +0100

    Add test files for monolingual PHP translations
    See https://github.com/nijel/weblate/issues/965
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ php-mono/en.php
@@ -0,0 +1,6 @@
+<?php
+
+$loc["hello"] = "Hello, world!\n";
+$loc["banana"] = "Orangutan has %d banana.\n";
+$loc["try"] = "Try Weblate at <http://demo.weblate.org/>!\n";
+$loc["thanks"] = "Thank you for using Weblate.";
